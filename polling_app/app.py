88fde8495363b06c88020b4f132729c77be1c04c
Chowdhury <md.nasif008@gmail.com>
2026-03-11 01:07:56 +0100

    basic callback

--- polling_app/app.py
+++ polling_app/app.py
@@ -1,23 +1,29 @@
 import dash 
 from dash import html
 from dash import dcc
-from dash.dependencies import Input, Output
+from dash import Input, Output
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.H1("Welcome to the dash Polling app!"),
     html.Div([
-        "Input: ", dcc.Input(id="input1", type="text", placeholder="initial value")
+        "Input: ", dcc.Input(id="input1", type="text", placeholder="initial value", value="") # input 
     ]),
     html.Br(),
-    html.Div(id="output-div")
+    html.Div(id="output-div") # output div
 ])
 
-@app.callback(Output(), input())
+@app.callback(
+    Output(component_id="output-div", component_property="children"), 
+    Input(component_id="input1", component_property="value")
+)
+def update_output(input_value):
+
+    output_val = f'Output: {input_value}'
+
+    return output_val
+
 
 if __name__ == "__main__":
     app.run(debug = True)
-
-
-
