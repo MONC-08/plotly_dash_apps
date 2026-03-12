82f75fdb7b57df8e71ec01f99944f7570db10b75
Chowdhury <md.nasif008@gmail.com>
2026-03-12 12:28:23 +0100

    multi page dash apps

--- polling_app/app.py
+++ polling_app/app.py
@@ -1,28 +1,55 @@
 import dash 
-from dash import html
-from dash import dcc
-from dash import Input, Output
+from dash import html, dcc, Input, Output
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H1("Welcome to the dash Polling app!"),
-    html.Div([
-        "Input: ", dcc.Input(id="input1", type="text", placeholder="initial value", value="") # input 
-    ]),
-    html.Br(),
+    # html.H1("Welcome to the dash Polling app!"),
+
+    dcc.Location(id="url", refresh=False),
+    
+    # dcc.Link("Home", href="/"),
+    # html.Br(),
+    # dcc.Link("polls", href="/polls"),
+    # html.Br(),
+    # dcc.Link("Poll Result", href="/polls_result"),
+
     html.Div(id="output-div") # output div
+])
+
+home_layout = html.Div(children=[
+    html.H1("Welcome to the dash Polling app!"),
+    dcc.Link("Home", href="/"),
+    html.Br(),
+    dcc.Link("polls", href="/polls"),
+    html.Br(),
+    dcc.Link("Poll Results", href="/polls_results")
+])
+
+polls_layout = html.Div(children=[
+    html.H1("This is the Poll page..."),
+    dcc.Link("Home", href="/")
+])
+
+polls_results_layout = html.Div(children=[
+    html.H1("Here is the Poll results..."),
+    dcc.Link("Home", href="/")
 ])
 
 @app.callback(
     Output(component_id="output-div", component_property="children"), 
-    Input(component_id="input1", component_property="value")
+    Input(component_id="url", component_property="pathname")
 )
-def update_output(input_value):
+def update_output(pathname):
 
-    output_val = f'Output: {input_value}'
-
-    return output_val
+    # output_val = f'Output: {input_value}'
+    if pathname == "/polls":
+        return polls_layout
+    elif pathname == "/polls_results":
+        return polls_results_layout
+    else:
+        return home_layout
+    
 
 
 if __name__ == "__main__":
